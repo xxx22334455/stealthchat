6039dbdbd581865ec4e500c64f7060239a01047c
StealthChat Dev <dev@stealthchat.local>
2026-05-29 16:04:17 +0400

    Change Android package name to avoid conflicts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.stealthchat"
+    namespace = "io.stealthchat.messenger"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.stealthchat"
+        applicationId = "io.stealthchat.messenger"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
